3f2e6b3fd293557a8313f49b7083165631983b01
Tarik Marton <88614520+Tamaritter@users.noreply.github.com>
2026-05-22 10:20:00 +0200

    Domänenmodell (#2)
    * Added models
    
    * Use Map for interpunktion in Document
    
    Replace the InterpunktiosProfil-typed field with Map<String, Long> to represent punctuation counts/frequencies. Remove the now-unused InterpunktiosProfil import and add java.util.Map import. Update getter and setter signatures to use Map<String, Long>.
    
    ---------
    
    Co-authored-by: kat <simonortlieb@gmx.de>

---
+++ src/main/java/de/lingoMetrics/Models/Wort.java
@@ -0,0 +1,50 @@
+package de.lingoMetrics.Models;
+
+import de.lingoMetrics.Enums.WortTyp;
+
+public class Wort {
+    private String inhalt;
+    private int laenge;
+    private boolean isSatzzeichen;
+    private WortTyp wortart;
+    private double sentiment;
+    private double konkretheit;
+    private boolean nominalStil;
+    private boolean verbalStil;
+    private boolean isFunktionsWort; // Im Diagramm als "isFunktionsWort: true" (Typ-Fehler im UML, hier boolean)
+    private boolean isFuellWort;       // Im Diagramm als "isFuellWort: true" (Typ-Fehler im UML, hier boolean)
+    private int vorkommenInText;
+
+    public String getInhalt() { return inhalt; }
+    public void setInhalt(String inhalt) { this.inhalt = inhalt; }
+
+    public int getLaenge() { return laenge; }
+    public void setLaenge(int laenge) { this.laenge = laenge; }
+
+    public boolean isSatzzeichen() { return isSatzzeichen; }
+    public void setSatzzeichen(boolean satzzeichen) { isSatzzeichen = satzzeichen; }
+
+    public WortTyp getWortart() { return wortart; }
+    public void setWortart(WortTyp wortart) { this.wortart = wortart; }
+
+    public double getSentiment() { return sentiment; }
+    public void setSentiment(double sentiment) { this.sentiment = sentiment; }
+
+    public double getKonkretheit() { return konkretheit; }
+    public void setKonkretheit(double konkretheit) { this.konkretheit = konkretheit; }
+
+    public boolean isNominalStil() { return nominalStil; }
+    public void setNominalStil(boolean nominalStil) { this.nominalStil = nominalStil; }
+
+    public boolean isVerbalStil() { return verbalStil; }
+    public void setVerbalStil(boolean verbalStil) { this.verbalStil = verbalStil; }
+
+    public boolean isFunktionsWort() { return isFunktionsWort; }
+    public void setFunktionsWort(boolean funktionsWort) { isFunktionsWort = funktionsWort; }
+
+    public boolean isFuellWort() { return isFuellWort; }
+    public void setFuellWort(boolean fuellWort) { isFuellWort = fuellWort; }
+
+    public int getVorkommenInText() { return vorkommenInText; }
+    public void setVorkommenInText(int vorkommenInText) { this.vorkommenInText = vorkommenInText; }
+}
--- src/main/java/de/lingoMetrics/Repository/DBword.java
+++ src/main/java/de/lingoMetrics/Repository/DBword.java
@@ -3,19 +3,19 @@ package de.lingoMetrics.Repository;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Word {
+public class DBword {
     final private String wort;
     final private Double value;
 
     @JsonCreator
-    public Word(@JsonProperty("wort") String wort,
-                @JsonProperty("value") Double value) {
+    public DBword(@JsonProperty("wort") String wort,
+                  @JsonProperty("value") Double value) {
         this.wort = wort;
         this.value = (value != null) ? value : 0.0;
     }
 
     @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-    public Word(String wort) {
+    public DBword(String wort) {
         this.wort = wort;
         this.value = 0.0;
     }
